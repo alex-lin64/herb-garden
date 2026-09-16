47a106757fb78f53bd42c02500cf356dab9ab29b
Alex <lin.sc.ph.bz@gmail.com>
2026-09-16 11:20:32 -0400

    set up tests

---
+++ test/test_native/test_helpers.cpp
@@ -0,0 +1,34 @@
+#include <unity.h>
+
+#include "../../src/Helpers/Helpers.h"
+
+void test_celsius_to_fahrenheit()
+{
+    TEST_ASSERT_FLOAT_WITHIN(
+        0.01f,
+        32.0f,
+        celsiusToFahrenheit(0.0f, 0.0f));
+
+    TEST_ASSERT_FLOAT_WITHIN(
+        0.01f,
+        77.0f,
+        celsiusToFahrenheit(25.0f, 0.0f));
+}
+
+void test_temperature_offset()
+{
+    TEST_ASSERT_FLOAT_WITHIN(
+        0.01f,
+        31.0f,
+        celsiusToFahrenheit(0.0f, -1.0f));
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_celsius_to_fahrenheit);
+    RUN_TEST(test_temperature_offset);
+
+    UNITY_END();
+}
