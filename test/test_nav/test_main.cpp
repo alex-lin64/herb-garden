ffaf3e7ae0cd0926e0aa36cff45202924d6e062a
Alex <lin.sc.ph.bz@gmail.com>
2026-09-16 22:44:53 -0400

    updated tests

--- test/test_nav/test_main.cpp
+++ test/test_nav/test_main.cpp
@@ -5,13 +5,15 @@
 #include "../../src/State/State.h"
 #include "../../src/UI/Navigation.cpp"
 
-void testCarouselRotateCW()
+void testHomeAutoRotateCW()
 {
     UIState ui;
     SystemState system;
 
+    system.settings.modeAuto = true;
+
     ui.carouselIndex = 0;
-    ui.screen = CAROUSEL_SCREENS_AUTO[0];
+    ui.screen = Screen::HOME;
 
     bool result = processInput(ui, system, InputEvent::ROTATE_CW);
 
@@ -22,10 +24,12 @@ void testCarouselRotateCW()
         static_cast<int>(ui.screen));
 }
 
-void testCarouselRotateCCW()
+void testHomeAutoRotateCCW()
 {
     UIState ui;
     SystemState system;
+
+    system.settings.modeAuto = true;
 
     ui.carouselIndex = 1;
     ui.screen = CAROUSEL_SCREENS_AUTO[1];
@@ -39,10 +43,12 @@ void testCarouselRotateCCW()
         static_cast<int>(ui.screen));
 }
 
-void testCarouselRotateCWWraps()
+void testHomeAutoRotateCWWraps()
 {
     UIState ui;
     SystemState system;
+
+    system.settings.modeAuto = true;
 
     ui.carouselIndex = CAROUSEL_SCREEN_COUNT_AUTO - 1;
     ui.screen = CAROUSEL_SCREENS_AUTO[ui.carouselIndex];
@@ -56,10 +62,12 @@ void testCarouselRotateCWWraps()
         static_cast<int>(ui.screen));
 }
 
-void testCarouselRotateCCWWraps()
+void testHomeAutoRotateCCWWraps()
 {
     UIState ui;
     SystemState system;
+
+    system.settings.modeAuto = true;
 
     ui.carouselIndex = 0;
     ui.screen = CAROUSEL_SCREENS_AUTO[0];
@@ -77,21 +85,143 @@ void testCarouselRotateCCWWraps()
         static_cast<int>(ui.screen));
 }
 
-void testCarouselBackReturnsToFirstPage()
+void testHomeManualRotateCW()
 {
     UIState ui;
     SystemState system;
 
-    ui.carouselIndex = 3;
-    ui.screen = CAROUSEL_SCREENS_AUTO[3];
+    system.settings.modeAuto = false;
 
-    bool result = processInput(ui, system, InputEvent::BACK);
+    ui.carouselIndex = 0;
+    ui.screen = Screen::HOME;
+
+    bool result = processInput(ui, system, InputEvent::ROTATE_CW);
+
+    TEST_ASSERT_TRUE(result);
+    TEST_ASSERT_EQUAL_INT(1, ui.carouselIndex);
+    TEST_ASSERT_EQUAL_INT(
+        static_cast<int>(CAROUSEL_SCREENS_MANUAL[1]),
+        static_cast<int>(ui.screen));
+}
+
+void testHomeManualRotateCCW()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = false;
+
+    ui.carouselIndex = 1;
+    ui.screen = CAROUSEL_SCREENS_MANUAL[1];
+
+    bool result = processInput(ui, system, InputEvent::ROTATE_CCW);
 
     TEST_ASSERT_TRUE(result);
     TEST_ASSERT_EQUAL_INT(0, ui.carouselIndex);
     TEST_ASSERT_EQUAL_INT(
-        static_cast<int>(CAROUSEL_SCREENS_AUTO[0]),
+        static_cast<int>(CAROUSEL_SCREENS_MANUAL[0]),
         static_cast<int>(ui.screen));
+}
+
+void testHomeManualRotateCWWraps()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = false;
+
+    ui.carouselIndex = CAROUSEL_SCREEN_COUNT_MANUAL - 1;
+    ui.screen = CAROUSEL_SCREENS_MANUAL[ui.carouselIndex];
+
+    bool result = processInput(ui, system, InputEvent::ROTATE_CW);
+
+    TEST_ASSERT_TRUE(result);
+    TEST_ASSERT_EQUAL_INT(0, ui.carouselIndex);
+    TEST_ASSERT_EQUAL_INT(
+        static_cast<int>(CAROUSEL_SCREENS_MANUAL[0]),
+        static_cast<int>(ui.screen));
+}
+
+void testHomeManualRotateCCWWraps()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = false;
+
+    ui.carouselIndex = 0;
+    ui.screen = CAROUSEL_SCREENS_MANUAL[0];
+
+    bool result = processInput(ui, system, InputEvent::ROTATE_CCW);
+
+    TEST_ASSERT_TRUE(result);
+    TEST_ASSERT_EQUAL_INT(
+        CAROUSEL_SCREEN_COUNT_MANUAL - 1,
+        ui.carouselIndex);
+
+    TEST_ASSERT_EQUAL_INT(
+        static_cast<int>(
+            CAROUSEL_SCREENS_MANUAL[CAROUSEL_SCREEN_COUNT_MANUAL - 1]),
+        static_cast<int>(ui.screen));
+}
+
+void testHomeBackReturnsFalse()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = true;
+
+    ui.carouselIndex = 2;
+    ui.screen = CAROUSEL_SCREENS_AUTO[2];
+
+    bool result = processInput(ui, system, InputEvent::BACK);
+
+    TEST_ASSERT_FALSE(result);
+}
+
+void testHomeRotaryPushReturnsFalse()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = true;
+
+    ui.carouselIndex = 2;
+    ui.screen = CAROUSEL_SCREENS_AUTO[2];
+
+    bool result = processInput(ui, system, InputEvent::ROTARY_PUSH);
+
+    TEST_ASSERT_FALSE(result);
+}
+
+void testHomeConfirmReturnsFalse()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = true;
+
+    ui.carouselIndex = 2;
+    ui.screen = CAROUSEL_SCREENS_AUTO[2];
+
+    bool result = processInput(ui, system, InputEvent::CONFIRM);
+
+    TEST_ASSERT_FALSE(result);
+}
+
+void testOtherScreensReturnFalse()
+{
+    UIState ui;
+    SystemState system;
+
+    system.settings.modeAuto = true;
+
+    ui.screen = Screen::LIGHTS;
+
+    bool result = processInput(ui, system, InputEvent::ROTATE_CW);
+
+    TEST_ASSERT_FALSE(result);
 }
 
 void setup()
@@ -100,11 +230,21 @@ void setup()
 
     UNITY_BEGIN();
 
-    RUN_TEST(testCarouselRotateCW);
-    RUN_TEST(testCarouselRotateCCW);
-    RUN_TEST(testCarouselRotateCWWraps);
-    RUN_TEST(testCarouselRotateCCWWraps);
-    RUN_TEST(testCarouselBackReturnsToFirstPage);
+    RUN_TEST(testHomeAutoRotateCW);
+    RUN_TEST(testHomeAutoRotateCCW);
+    RUN_TEST(testHomeAutoRotateCWWraps);
+    RUN_TEST(testHomeAutoRotateCCWWraps);
+
+    RUN_TEST(testHomeManualRotateCW);
+    RUN_TEST(testHomeManualRotateCCW);
+    RUN_TEST(testHomeManualRotateCWWraps);
+    RUN_TEST(testHomeManualRotateCCWWraps);
+
+    RUN_TEST(testHomeBackReturnsFalse);
+    RUN_TEST(testHomeRotaryPushReturnsFalse);
+    RUN_TEST(testHomeConfirmReturnsFalse);
+
+    RUN_TEST(testOtherScreensReturnFalse);
 
     UNITY_END();
 }
